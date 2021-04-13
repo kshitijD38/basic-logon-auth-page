fbe1399437af8bd43cf9abcdc406c2f7986c078d
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-04-13 17:16:31 +0530

    message

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home({ handleEmail, handlePassword, error1 }) {
       {!error1 && (
         <h4> hi new logged in user {window.localStorage.getItem("Email")}</h4>
       )}
-      {error1 && <h4>error: Please input email and password</h4>}
+      {error1 && <h4>error: Please input proper email and password</h4>}
     </>
   );
 }
